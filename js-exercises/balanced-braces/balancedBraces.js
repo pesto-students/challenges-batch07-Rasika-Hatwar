8be741edcdaa3bea92428f351160f9f3ea783f0a
Rasika Hatwar <rasikahatwar13@gmail.com>
2021-02-25 01:11:53 +0530

    Update solution for balanced braces

--- js-exercises/balanced-braces/balancedBraces.js
+++ js-exercises/balanced-braces/balancedBraces.js
@@ -1,5 +1,19 @@
-function balancedBraces(...args) {
-  return args;
+function balancedBraces(str) {
+  const bracket = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+  const stack = [];
+
+  for (const char of str) {
+    if (bracket[char]) {
+      stack.push(char);
+    } else if (Object.values(bracket).includes(char) && char !== bracket[stack.pop()]) {
+      return false;
+    }
+  }
+  return !stack.length;
 }
 
 export {
